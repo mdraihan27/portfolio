47256630355010cfd26f53a9f06122677e0e2302
mdraihan27 <mdraihanhossen.cse@gmail.com>
2025-08-30 04:04:00 +0600

    Add all files

---
+++ app/layout.js
@@ -0,0 +1,36 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import InteractiveBackground from "./components/InteractiveBackground";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Md. Raihan Hossen | Portfolio",
+  description: "Portfolio of Md. Raihan Hossen",
+  icons: {
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
+    apple: "/icon.svg",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+  className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+  <InteractiveBackground />
+  {children}
+      </body>
+    </html>
+  );
+}
